a139f1eca4149007d88b6e4fb4bd7fa09822d23a
Gregory Daynes <gregdaynes@gmail.com>
2021-05-22 23:01:33 -0700

    chore(app): Basic server and app structure
    Boilerplate for fastify web server, no plugins or routes defined yet.

---
+++ src/index.test.js
@@ -0,0 +1,17 @@
+const { test } = require("tap")
+const build = require("./index")
+
+test("boots the app", async (t) => {
+  t.plan(1)
+
+  t.teardown(() => app.close())
+  const app = await build()
+
+  await app.addHook("onReady", async () => {
+    t.ok(true)
+  })
+
+  await app.ready()
+
+  t.end()
+})
