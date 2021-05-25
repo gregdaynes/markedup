8c977e0628ed1555bd085e9eb434529d9e2f4e1b
Gregory Daynes <gregdaynes@gmail.com>
2021-05-25 15:11:00 -0700

    feature(app): onRequest tag timestamp
    Implement a hook for onRequest that puts the current timestamp utc in MS
    in the threads Async hook storage.

---
+++ src/plugins/request-context.js
@@ -0,0 +1,11 @@
+const fastifyPlugin = require("fastify-plugin")
+const { fastifyRequestContextPlugin } = require("fastify-request-context")
+
+module.exports = fastifyPlugin(async (app) => {
+  app.register(fastifyRequestContextPlugin, {
+    hook: "onRequest",
+    defaultStoreValues: {
+      timestamp: new Date().valueOf(),
+    },
+  })
+})
